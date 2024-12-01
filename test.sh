c3b737f3fe4ab0dce9a7ef5cf3d83fc84b3cf5fd
TopeEstLa <contact.tope95@gmail.com>
2024-12-01 14:09:11 +0100

    add case in test.sh

--- test.sh
+++ test.sh
@@ -37,3 +37,23 @@ if [ $STATUS_CODE != 0 ]; then
     exit 1
 fi
 
+./$EXEC_NAME test-data.csv lv all 1
+STATUS_CODE=$?
+if [ $STATUS_CODE != 0 ]; then
+    echo "Test failed exit code $STATUS_CODE"
+    exit 1
+fi
+
+./$EXEC_NAME test-data.csv hvb comp 2
+STATUS_CODE=$?
+if [ $STATUS_CODE != 0 ]; then
+    echo "Test failed exit code $STATUS_CODE"
+    exit 1
+fi
+
+./$EXEC_NAME test-data.csv hva comp 3
+STATUS_CODE=$?
+if [ $STATUS_CODE != 0 ]; then
+    echo "Test failed exit code $STATUS_CODE"
+    exit 1
+fi
