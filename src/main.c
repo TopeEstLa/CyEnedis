ac0fee5877b630ccb0e533434ca0adc56c55e491
TopeEstLa <contact.tope95@gmail.com>
2024-12-03 20:38:41 +0100

    clean main file

--- src/main.c
+++ src/main.c
@@ -32,15 +32,10 @@ int main(int argc, char *argv[]) {
     mark_benchmark_start();
     ApplicationSettings *settings = parse_application_settings(argc, argv);
     if (settings == NULL) {
-        printf("Could not parse settings\n");
-        printf("Usage: %s <filename> <station_type> <consumer_type> [power_plant]\n", argv[0]);
         return 2;
     }
 
     if (!validate_application_settings(settings)) {
-        printf("Invalid settings\n");
-        printf("Usage: %s <filename> <station_type> <consumer_type> [power_plant]\n", argv[0]);
-        free_application_settings(settings);
         return 3;
     }
 
@@ -48,18 +43,14 @@ int main(int argc, char *argv[]) {
 
     StationNode *node = process_csv_file(settings);
     if (node == NULL) {
-        printf("Error processing file\n");
-        free_application_settings(settings);
         return 4;
     }
+
     mark_build_time();
 
     int count = 0;
     StationResult **results = collect_results(node, &count);
     if (results == NULL) {
-        printf("Error collecting results\n");
-        free_application_settings(settings);
-        free_station_node(node);
         return 5;
     }
     mark_collect_time();
@@ -71,12 +62,6 @@ int main(int argc, char *argv[]) {
 
     char *filename = generate_output_filename(settings);
     if (filename == NULL) {
-        printf("Error generating filename\n");
-        free_application_settings(settings);
-        for (int i = 0; i < count; i++) {
-            free_station_result(results[i]);
-        }
-        free(results);
         return 6;
     }
 
@@ -84,21 +69,10 @@ int main(int argc, char *argv[]) {
     mark_write_time();
 
     if (settings->station_type == STATION_LV && settings->consumer_type == CONSUMER_ALL) {
-        printf("Case of LV and all consumers generating minmax file\n");
         qsort_by_load(results, count);
 
         char *minmax_filename = generate_minmax_output_filename(settings);
         if (minmax_filename == NULL) {
-            printf("Error generating minmax filename\n");
-
-            free_application_settings(settings);
-
-            for (int i = 0; i < count; i++) {
-                free_station_result(results[i]);
-            }
-
-            free(results);
-            free(filename);
             return 7;
         }
 
@@ -116,7 +90,6 @@ int main(int argc, char *argv[]) {
 
     free_application_settings(settings);
 
-    //clear terminal
     mark_benchmark_end();
     benchmark_result();
     return 0;
