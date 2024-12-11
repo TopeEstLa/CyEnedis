04f6a98f3c40e3839c21d03e4fe3a3448a3a441a
RicardoKillos <167462875+RicardoKillos@users.noreply.github.com>
2024-12-11 14:00:55 +0100

    add comment on waiting scene

--- codeC/include/waiting_scene.h
+++ codeC/include/waiting_scene.h
@@ -1,8 +1,15 @@
 #ifndef WAITING_SCENE_H
 #define WAITING_SCENE_H
 
+/**
+@brief Init waiting scene system
+ */
 void init_waiting_scene();
 
+/**
+@brief Loop for rendering the waiting scene
+@param varg the thread argument
+ */
 void* draw_waiting_scene(void* varg);
 
 #endif //WAITING_SCENE_H
