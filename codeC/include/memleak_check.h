82f37ae4b7c85c2a3eb2ed8bdff72fc0db1b483f
RicardoKillos <167462875+RicardoKillos@users.noreply.github.com>
2024-12-11 14:30:38 +0100

    add comment on mem leak function

--- codeC/include/memleak_check.h
+++ codeC/include/memleak_check.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+* @brief structure representing allocation tracking
+ */
 typedef struct Allocation {
     void *ptr;
     size_t size;
@@ -13,14 +16,29 @@ typedef struct Allocation {
     struct Allocation *next;
 } Allocation;
 
+/**
+* @brief add new allocation to track
+ */ 
 void add_allocation(void *ptr, size_t size, const char *file, int line);
 
+/**
+* @brief remove tracked allocation
+ */
 void remove_allocation(void *ptr);
 
+/**
+* @brief check if all tracked allocation as been removed
+ */
 void check_leaks();
 
+/**
+* @brief a wrapper for malloc function to track all alocation
+ */
 void *malloc_wrapper(size_t size, const char *file, int line);
 
+/**
+* @brief a wrapper for free function to track all alocation remove
+ */
 void free_wrapper(void *ptr);
 
 #endif //MEMLEAK_CHECK_H
