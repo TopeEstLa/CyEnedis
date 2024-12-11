80ecf310bb5b86613661ad371bf6519d0306b733
RicardoKillos <167462875+RicardoKillos@users.noreply.github.com>
2024-12-11 14:32:57 +0100

    add comment on benchmark

--- codeC/include/benchmark.h
+++ codeC/include/benchmark.h
@@ -12,22 +12,56 @@
 #include <sys/resource.h>
 #endif
 
+/**
+enable benchmark method
+ */
 void set_benchmark_enabled(bool enabled);
 
+/**
+get the current time in millis
+ */
 long long current_time_in_ms();
 
+/**
+mark the start of a benchmark
+ */
 void mark_benchmark_start();
 
+/**
+mark the end of a benchmark
+ */
 void mark_benchmark_end();
 
+/**
+mark the end of settings time process
+ */
 void mark_setting_time();
+
+/**
+mark the end of the build time process
+ */
 void mark_build_time();
+/**
+mark the end of the collect time process
+ */
 void mark_collect_time();
+/*
+mark the end of the sort time process
+*/
 void mark_sort_time();
+/**
+mark the end of the write time process
+ */
 void mark_write_time();
 
+/**
+pretty print of the benchmark result
+ */
 void benchmark_result();
 
+/**
+print the memory usage of the program
+ */
 void print_memory_usage();
 
 #endif //BENCHMARK_H
